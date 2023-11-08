44ab5ba4aca8fb000d2a572acc1c0f3befe9d258
luigitarallo <tarallo.gigi@gmail.com>
2023-11-08 22:39:13 +0100

    feat: add projecsByType function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Type;
 use App\Models\Project;
 
 class ProjectController extends Controller
@@ -34,5 +35,22 @@ class ProjectController extends Controller
             ->with('technologies:id,name,color', 'type:id,name,color')
             ->first();
         return response()->json($project);
+    }
+
+    public function projectsByType($type_id)
+    {
+        $type = Type::select('id', 'name', 'color')
+            ->where('id', $type_id)
+            ->first();
+
+        $projects = Project::select('id', 'name', 'type_id', 'description', 'link', 'slug')
+            ->where('type_id', $type_id)
+            ->with('technologies:id,name,color', 'type:id,name,color')
+            ->orderByDesc('id')
+            ->get();
+        return response()->json([
+            'type' => $type,
+            'projects' => $projects
+        ]);
     }
 }
